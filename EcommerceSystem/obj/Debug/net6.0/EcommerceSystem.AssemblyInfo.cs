1e89a9b80f91bc9ad1082f5299e63a0794a22ed2
chamalkaMarasinghe <chamalka@icieos.com>
2024-10-14 21:17:01 +0530

    conflict resolve 3

--- EcommerceSystem/obj/Debug/net6.0/EcommerceSystem.AssemblyInfo.cs
+++ EcommerceSystem/obj/Debug/net6.0/EcommerceSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9586638907b4db94ddd6fa72f8532d8e31235069")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6594950c1a5cd861419a0fe96212268ef28db4fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
